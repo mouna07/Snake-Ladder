30285c104cccc643f2ca714a663c4382c2a51e6f
mouna07 <mounamc261@gmail.com>
2021-02-02 12:46:02 +0530

    ADDED:winnig position

--- snakeladder.java
+++ snakeladder.java
@@ -1,24 +1,31 @@
 public class snakeladder{
  	static final int initialPosition = 0;
- 	public static void main(String[]args){
-  	System.out.println("****Welcome to snakeLadder simulation****");
-  	int numberOfPosition=0;
-  	int playPosition=0;
-  	int rollDice =(int) Math.floor(Math.random()*6+1);
-  	int checkForOption=(int) Math.floor(Math.random()*10)%3;
+    	static final int winingPosition = 100;
+    	static final int ladder = 1;
+    	static final int snake = 2;
 
-  	switch(checkForOption){
-     		case 1:
-      			System.out.println("Ladder");
-      			playPosition=playPosition+rollDice;
-      			System.out.println("play position : " + playPosition);
-      			break;
-     		case 2:
-      			System.out.println("Snake");
-      			playPosition=playPosition-rollDice;
-      			System.out.println("play position : " + playPosition);
-      			break;
-     		default:
-      			System.out.println("No Play");
+    	public static void main(String[] args) {
+        	System.out.println("Welcome to snakeLadder simulation");
+        	int playPosition = 0;
+        	boolean validation = true;
+        	while (validation) {
+            		int rollDice = (int) Math.floor(Math.random() * 6 + 1);
+            		int checkForOption = (int) Math.floor(Math.random() * 10) % 3;
+            		if (checkForOption == ladder) {
+                		playPosition = playPosition + rollDice;
+            		}
+			else if (checkForOption == snake) {
+                		playPosition = playPosition - rollDice;
+                		if(playPosition<0){
+                    			playPosition=initialPosition;
+                	}
+            	}
+                if (playPosition == winingPosition) {
+                	System.out.println("win");
+                	break;
+            	}
+
+        }
+
     }
 }
