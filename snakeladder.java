1c097704b719b491f483bace259e85ab722c5bfa
mouna07 <mounamc261@gmail.com>
2021-02-02 12:33:17 +0530

    check for option

--- snakeladder.java
+++ snakeladder.java
@@ -2,8 +2,23 @@ public class snakeladder{
  	static final int initialPosition = 0;
  	public static void main(String[]args){
   	System.out.println("****Welcome to snakeLadder simulation****");
-  	System.out.println("snake&ladder start with single player at start pos : " + initialPosition);
-	int rollDice =(int) Math.floor(Math.random()*6+1);
-  	System.out.println("player roll a dice: " + rollDice);
+  	int numberOfPosition=0;
+  	int playPosition=0;
+  	int rollDice =(int) Math.floor(Math.random()*6+1);
+  	int checkForOption=(int) Math.floor(Math.random()*10)%3;
+
+  	switch(checkForOption){
+     		case 1:
+      			System.out.println("Ladder");
+      			playPosition=playPosition+rollDice;
+      			System.out.println("play position : " + playPosition);
+      			break;
+     		case 2:
+      			System.out.println("Snake");
+      			playPosition=playPosition-rollDice;
+      			System.out.println("play position : " + playPosition);
+      			break;
+     		default:
+      			System.out.println("No Play");
     }
 }
