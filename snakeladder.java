d591d3abbc2f0aaf71513ad47cc2cd15fc3f60cf
mouna07 <mounamc261@gmail.com>
2021-02-02 12:28:35 +0530

    the roll dice

--- snakeladder.java
+++ snakeladder.java
@@ -3,5 +3,7 @@ public class snakeladder{
  	public static void main(String[]args){
   	System.out.println("****Welcome to snakeLadder simulation****");
   	System.out.println("snake&ladder start with single player at start pos : " + initialPosition);
+	int rollDice =(int) Math.floor(Math.random()*6+1);
+  	System.out.println("player roll a dice: " + rollDice);
     }
 }
