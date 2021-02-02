fcc7428f23209d2382c194731a6cd3c536f49a99
mouna07 <mounamc261@gmail.com>
2021-02-02 13:05:01 +0530

    the dice count

--- snakeladder.java
+++ snakeladder.java
@@ -7,16 +7,18 @@ public class snakeladder{
     	public static void main(String[] args) {
         	System.out.println("Welcome to snakeLadder simulation");
         	int playPosition = 0;
+        	int diceCount=0;
         	boolean validation = true;
         	while (validation) {
             		int rollDice = (int) Math.floor(Math.random() * 6 + 1);
             		int checkForOption = (int) Math.floor(Math.random() * 10) % 3;
             		if (checkForOption == ladder) {
                 		playPosition = playPosition + rollDice;
+                		diceCount=diceCount+1;
                 		if (playPosition >100){
                     			playPosition=100;
                 		}
-            		}
+            		} 
 			else if (checkForOption == snake) {
                 		playPosition = playPosition - rollDice;
                			if(playPosition < initialPosition){
@@ -27,15 +29,17 @@ public class snakeladder{
                			}
                		}
 
-
-         		if (playPosition < winingPosition) {
+			System.out.println("playPosition: " +playPosition );
+            		if (playPosition < winingPosition) {
                 		playPosition = playPosition;
-			}
+			} 
 			else if (playPosition == winingPosition) {
                 		System.out.println(" Exact win");
+                		System.out.println("diceCount:" + diceCount);
 				break;
             		}
 		}
 	}
 }
+
 
